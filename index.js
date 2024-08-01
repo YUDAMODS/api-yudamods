ceb17985d816f2ab4fe3f32aa498bd8d8a2c4172
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-08-01 15:25:13 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var express = require('express'),
   cors = require('cors'),
   secure = require('ssl-express-www');
-const PORT = process.env.PORT || 3000 || 5000 || 8080
-var { color } = require('./lib/color.js')
-var mainrouter = require('./routes/main.js'),
-  apirouter = require('./routes/api.js')
+const PORT = process.env.PORT || 3000 || 5000 || 8080;
+var { color } = require('./lib/color.js');
+//var mainrouter = require('./routes/main.js'),
+  apirouter = require('./routes/api.js');
 
-var app = express()
+var app = express();
 app.enable('trust proxy');
-app.set("json spaces",2)
-app.use(cors())
-app.use(secure)
-app.use(express.static("lib"))
+app.set("json spaces", 2);
+app.use(cors());
+app.use(secure);
+app.use(express.static("lib"));
+app.use(express.static("public")); // Menambahkan baris ini untuk melayani file statis dari folder public
 
-
-app.use('/', mainrouter)
-app.use('/api', apirouter)
+//app.use('/', mainrouter);
+app.use('/api', apirouter);
 app.use(function (req, res, next) {
-  res.redirect('https://github.com/Xorizn/xorizn-apis#readme')
+  res.sendFile(__dirname + '/public/index.html'); // Mengarahkan ke index.html
 });
 
 app.listen(PORT, () => {
-  console.log(color("Server running on port " + PORT,'green'))
-})
+  console.log(color("Server running on port " + PORT, 'green'));
+});
 
-module.exports = app
+module.exports = app;
